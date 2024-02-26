cb57d96f891f0db20c4658c64e450e06f43b926e
lumisong <xiaohuzhenskr@gmail.com>
2024-02-26 22:26:35 +0800

    修改 部分md关于数据代码部分

--- lithology_competition/data/test_code.py
+++ lithology_competition/data/test_code.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Load test data
 hidden_test_data = pd.read_csv('hidden_test.csv', sep=';')
@@ -28,7 +29,7 @@ def score(y_true, y_pred):
     S = 0.0
     y_true = y_true.astype(int)
     y_pred = y_pred.astype(int)
-    for i in range(0, y_true.shape[0]):
+    for i in range(y_true.shape[0]):
         S -= A[y_true[i], y_pred[i]]
     return S/y_true.shape[0]
 score(test_true_y, test_prediction_y)
